a74b094937aa12268bde1fb01ab25fae03d27201
darareaksmey-heng <darareaksmey.heng@mjqeducation.edu.kh>
2022-08-29 15:48:43 +0700

    website_mentor_setup

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Sticky Navbar
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 50) {
+        $(".sticky").addClass("nav-sticky");
+    } else {
+        $(".sticky").removeClass("nav-sticky");
+    }
+});
